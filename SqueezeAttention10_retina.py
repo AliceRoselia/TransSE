ce15426a176822be53534eefb9721e3fb41819a0
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-08-20 14:20:23 +0700

    Try a bunch of things.

--- SqueezeAttention10_retina.py
+++ SqueezeAttention10_retina.py
@@ -175,11 +175,11 @@ class SqueezeAttention(nn.Module):
         self.UP1 = UpProjection(32, 64)
         self.UP2 = UpProjection(64, 128)
         self.UP3 = UpProjection(128, 256)
-        self.UP_OUT = UpProjection(256, 1024)
+        self.UP_OUT = UpProjection(256, 256)
         
         self.dropout = nn.Dropout(0.25)
         
-        self.results = nn.Linear(8192, classes)
+        self.results = nn.Linear(2048, classes)
     
     def forward(self,x):
         B,C,H,W = x.shape
@@ -207,7 +207,7 @@ class SqueezeAttention(nn.Module):
         
         
         
-        x = self.dropout(x.mean((3,4)).view(-1,8192))
+        x = self.dropout(x.mean((3,4)).view(-1,2048))
         
         return self.results(x)
         
@@ -292,13 +292,13 @@ if __name__ == "__main__":
         if correct > best:
             best = correct
             print("New frontier reached.")
-            torch.save(net.state_dict(),"Retina_SqueezeAttention29_1.pt")
+            torch.save(net.state_dict(),"Retina_SqueezeAttention32_1.pt")
 
 
 #This section is deliberately separate in case we want to just evaluate the model.
 
 if __name__ == "__main__":
-    pretrained = torch.load("Retina_SqueezeAttention29_1.pt") #Let's get up to 10 epochs?
+    pretrained = torch.load("Retina_SqueezeAttention32_1.pt") #Let's get up to 10 epochs?
     net.load_state_dict(pretrained)
 
 
@@ -441,3 +441,13 @@ if __name__ == "__main__":
 #Version 26: 0.6125 (try up-sizing output)
 
 #Version 27: 0.5775 (More dropout-worse?)
+
+#Version 28: 0.635 (weight decay out = 0.005)
+
+#Version 29: 0.6075
+
+#Version 30: 0.6225
+
+#Version 31: 0.645
+
+#Version 32: 0.6175 (High validation BUT low real accuracy?)
--- SqueezeAttention8_retina.py
+++ SqueezeAttention8_retina.py
@@ -224,7 +224,7 @@ param_groups = [
     dict(params=hidden_weights, use_muon=True,
          lr=0.01, weight_decay=0.01),
     dict(params=hidden_gains_biases+nonhidden_params, use_muon=False,
-         lr=1.5e-4, betas=(0.9, 0.99), weight_decay=0.001),
+         lr=1.5e-4, betas=(0.9, 0.99), weight_decay=0.01),
 ]
 optimizer = SingleDeviceMuonWithAuxAdam(param_groups)
 
@@ -245,18 +245,18 @@ best = 0
 #pretrained = torch.load("Retina_SqueezeAttention6_1.pt") #Let's get up to 10 epochs?
 #net.load_state_dict(pretrained)
 
-"""
+
 max_epoch = 20
-muon_max = 0.02
-muon_min = 0.01
-adam_max = 3e-4
-adam_min = 1.5e-4
+#muon_max = 0.02
+#muon_min = 0.01
+#adam_max = 3e-4
+#adam_min = 1.5e-4
 
 if __name__ == "__main__":
     for epoch in range(max_epoch):
         
         print("Current epoch:",epoch+1)
-        schedule_LR(optimizer, epoch, max_epoch-1, muon_max, muon_min, adam_max, adam_min)
+        #schedule_LR(optimizer, epoch, max_epoch-1, muon_max, muon_min, adam_max, adam_min)
         net.train()
         batch = 0
         for data_input, result in train_data_loader:
@@ -288,14 +288,14 @@ if __name__ == "__main__":
         if correct > best:
             best = correct
             print("New frontier reached.")
-            torch.save(net.state_dict(),"Retina_SqueezeAttention25_1.pt")
+            torch.save(net.state_dict(),"Retina_SqueezeAttention33_1.pt")
 
-"""
+
 
 #This section is deliberately separate in case we want to just evaluate the model.
 
 if __name__ == "__main__":
-    pretrained = torch.load("Retina_SqueezeAttention19_1.pt") #Let's get up to 10 epochs?
+    pretrained = torch.load("Retina_SqueezeAttention33_1.pt") #Let's get up to 10 epochs?
     net.load_state_dict(pretrained)
 
 
@@ -423,7 +423,7 @@ if __name__ == "__main__":
 
 
 #Heard you like param tunes? 0.5975 with weight decay = 0.02. Wait... wrong... that was lr=0.02
-# The best one is with lr = 0.01 and 0.001. (Version 19)
+# The best one is with lr = 0.01 and 0.001. (Version 19), 0.655
 
 #With lr = 0.02 on both: 0.6425
 
@@ -434,3 +434,5 @@ if __name__ == "__main__":
 #Version 24: 0.6025
 
 #Version 25: 0.595
+
+#Version 33: 0.585
