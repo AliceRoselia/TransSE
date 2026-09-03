53edc950ed5e9cecb90a1bca9458d3ffb9a772fb
Myself <63040919+AliceRoselia@users.noreply.github.com>
2026-09-03 18:12:39 +0700

    Pneumonia!

--- SqueezeAttention11_pneumonia.py
+++ SqueezeAttention11_pneumonia.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as func
 
-from medmnist import BreastMNIST
+from medmnist import PneumoniaMNIST
 import torchvision.transforms as transforms
 import torch.utils.data as data
 from muon import SingleDeviceMuonWithAuxAdam
@@ -20,21 +20,14 @@ torch._dynamo.config.accumulated_cache_size_limit = 128
 
 #from torch.nn.attention import SDPBackend, sdpa_kernel
 
-torch.manual_seed(71203674)
+torch.manual_seed(45768742)
 
 torch.set_float32_matmul_precision("high")
 
 
 #TransSEnet for medical imaging and similar tasks.
-import math
 
-def schedule_LR(optimizer, epoch, max_epoch, muon_max, muon_min, adam_max, adam_min):
-    #Cosine annealing. Self-implemented because the Pytorch version can't accept when there are 2 versions.
-    for group in optimizer.param_groups:
-        if group["use_muon"]:
-            group["lr"] = muon_min + (1+math.cos(math.pi*epoch/max_epoch))*(muon_max-muon_min)/2
-        else:
-            group["lr"] = adam_min + (1+math.cos(math.pi*epoch/max_epoch))*(adam_max-adam_min)/2
+
     
 
         
@@ -44,9 +37,9 @@ def schedule_LR(optimizer, epoch, max_epoch, muon_max, muon_min, adam_max, adam_
     
 batch_size = 2
 num_workers = 4
-prefetch_factor = 40 
+prefetch_factor = 8 
 
-train_data = BreastMNIST(split="train",transform = transforms.Compose([
+train_data = PneumoniaMNIST(split="train",transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomRotation(15),
@@ -56,11 +49,11 @@ train_data = BreastMNIST(split="train",transform = transforms.Compose([
 train_data_loader = data.DataLoader(dataset = train_data, batch_size = batch_size,shuffle = True,
 pin_memory=True,num_workers=num_workers,prefetch_factor=prefetch_factor,persistent_workers=True)
 
-val_data = BreastMNIST(split="val",transform = transforms.ToTensor(),download=True,size = 224)
+val_data = PneumoniaMNIST(split="val",transform = transforms.ToTensor(),download=True,size = 224)
 val_data_loader = data.DataLoader(dataset = val_data, batch_size = batch_size,shuffle = True,
 pin_memory=True,num_workers=num_workers,prefetch_factor=prefetch_factor,persistent_workers=True)
 
-test_data = BreastMNIST(split="test",transform = transforms.ToTensor(),download=True,size = 224)
+test_data = PneumoniaMNIST(split="test",transform = transforms.ToTensor(),download=True,size = 224)
 test_data_loader = data.DataLoader(dataset = test_data, batch_size = batch_size,shuffle = False,
 pin_memory=True,num_workers=num_workers,prefetch_factor=prefetch_factor,persistent_workers=True)
 
@@ -92,7 +85,7 @@ class SqueezeAttentionBlock(nn.Module):
         #return self.pw_conv(self.dw_conv(x))
     
     
-    #@torch.compile()
+    @torch.compile()
     def forward(self,x):
         # X is of shape [B,M,N,H,W]
         B,M,N,H,W = x.shape
@@ -169,8 +162,7 @@ class SqueezeAttention(nn.Module):
         self.SAB14 = SqueezeAttentionBlock(8, 512)
         self.SAB15 = SqueezeAttentionBlock(8, 512)
         self.SAB16 = SqueezeAttentionBlock(8, 512)
-        #self.SAB17 = SqueezeAttentionBlock(8, 512)
-        #self.SAB18 = SqueezeAttentionBlock(8, 512)
+        
         
         
         self.UP1 = UpProjection(32, 64)
@@ -181,7 +173,7 @@ class SqueezeAttention(nn.Module):
         self.dropout = nn.Dropout(0.25)
         
         self.results = nn.Linear(4096, classes)
-    @torch.compile()
+    
     def forward(self,x):
         B,C,H,W = x.shape
         x = self.intro(x).view(B,8,32,H,W)
@@ -209,8 +201,7 @@ class SqueezeAttention(nn.Module):
         x = self.SAB14(x)
         x = self.SAB15(x)
         x = self.SAB16(x)
-        #x = self.SAB17(x)
-        #x = self.SAB18(x)
+        #x = self.squeeze_to_pool(x) #14
         
         
         
@@ -231,24 +222,6 @@ net = SqueezeAttention(1, 2).to("cuda")
 hidden_weights = [p for p in net.parameters() if p.ndim >= 2][1:-1]
 hidden_gains_biases = [p for p in net.parameters() if p.ndim < 2]
 nonhidden_params = [net.intro.weight, net.results.weight]
-"""
-hyperparams = [0.0011726408837611168, 0.010916422693267544, 0.00012082952436437763,
-               0.09625209551306378, 0.012484844030091051, 0.007170883440335636, 
-               2.918680541912039e-05, 0.1571406473657313, 0.005553990819302258, 0.07000432048238599]
-
-
-
-hyperparams = [0.0022443332150382162, 0.005144466831288332, 3.233003581006977e-05,
-                        0.04954685101372992, 0.01419429600788806, 0.0017545040377340594,
-                        1.423908427033411e-05, 0.3231503419580622, 0.0024723986019527834,
-                        0.03852306426142474]
-
-
-hyperparams = [0.0010889095024196832,0.009942881373920073,0.00012181194634217088,0.09829253245193824,0.012792986806694356,
- 0.009437247084548201,5.167431478097197e-05,0.10057588836850961,0.009492422493706855,0.10705255784270944]
-"""
-
-#hyperparams = [0.002215482342290458, 0.005105138289552276, 1.2315904272962708e-05, 0.08801671039700643, 0.008528549554398248, 0.003066647909077181, 1.2325917306245926e-05, 0.17869996096493151, 0.002044691123908544, 0.04567306769116592]
 hyperparams = [0.0008602522078379203, 0.012369705667362903, 9.752763156138204e-05, 0.0655110378721131, 0.013811079331863048, 0.010340056669188179, 1.9917593041199128e-05, 0.1769209451676188, 0.007003697900328614, 0.056487999850367676]
 param_groups = [
     dict(params=hidden_weights, use_muon=True,
@@ -274,26 +247,20 @@ loss = nn.CrossEntropyLoss()
 
 best = 0
 
-#pretrained = torch.load("Retina_SqueezeAttention6_1.pt") #Let's get up to 10 epochs?
-#net.load_state_dict(pretrained)
+max_epoch = 30
 
-max_epoch = 50
-#muon_max = 0.02
-#muon_min = 0.005
-#adam_max = 3e-4
-#adam_min = 7.5e-5
+
+
 
 if __name__ == "__main__":
     for epoch in range(max_epoch):
-        
         print("Current epoch:",epoch+1)
-        #schedule_LR(optimizer, epoch, max_epoch-1, muon_max, muon_min, adam_max, adam_min)
+    
         net.train()
-        #batch = 0
-        
+        batch = 0
         for data_input, result in train_data_loader:
-            #batch += 1
-            #print("batch:",batch)
+            batch += 1
+            print("batch:",batch)
             result = result.to("cuda",non_blocking = True)
             prediction = net(data_input.to("cuda",non_blocking = True))
             result_loss = loss(prediction,result.view(-1))
@@ -301,6 +268,10 @@ if __name__ == "__main__":
             
             optimizer.step()
             optimizer.zero_grad()
+            
+            
+            
+            #print(result_loss)
         
         net.eval()
         correct = 0
@@ -316,19 +287,19 @@ if __name__ == "__main__":
         if correct > best:
             best = correct
             print("New frontier reached.")
-            torch.save(net.state_dict(),"Breast_SqueezeAttention51_1.pt")
+            torch.save(net.state_dict(),"Pneumonia_SqueezeAttention6_1.pt")
 
 
 
 #This section is deliberately separate in case we want to just evaluate the model.
 
 if __name__ == "__main__":
-    pretrained = torch.load("Breast_SqueezeAttention51_1.pt") #Let's get up to 10 epochs?
+    pretrained = torch.load("Pneumonia_SqueezeAttention6_1.pt") #Let's get up to 10 epochs?
     net.load_state_dict(pretrained)
 
 
     correct = 0
-    total = 156 
+    total = 624 
         
     net.eval()
     with torch.no_grad():
@@ -474,36 +445,11 @@ if __name__ == "__main__":
 
 #Label smoothing = 0.05: 0.8654
 
-#With norm: 0.8269
 
-#With norm in a different way: 0.8526
+#Trying pneumonia mnist: 0.8558
 
-#Weight decay = 0.01: 0.8462
+#Attempt 2 without the squeeze at the end: 0.867
 
-#Lower data augmentation: 0.8782
+#Attempt 3 (more layers): 0.8638
 
-#With weight decay = 0.03: 0.8782
-
-#The best one is weight decay = (0.02, 0.002) (version 38.)
-
-#With weight decay = (0.02,0.005) and adjusted betas... not working. 0.833
-
-#With the adjusted betas alone... 0.8653. Not working.
-
-#With weight decay = (0.02, 0.002): 0.8717
-
-#With weight decay = (0.01, 0.001): 0.8910
-
-#New one: 0.8974 (version 45)
-
-#New one: 0.8526 (version 46)
-
-#Version 47: 0.8846153846153846
-
-#Version 48: 0.8397435897435898
-
-#Version 49: 0.8782051282051282
-
-#Version 50: 0.8846153846153846
-
-#Version 51: 0.8782051282051282
+#Attept 4 (normalize): 0.8798: YAY!
